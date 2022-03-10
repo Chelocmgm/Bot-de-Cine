c9dee2afdf332aab84791abc8d7e5767e2cc7558
fazt <contact@faztweb.com>
2022-03-09 20:26:04 -0400

    Agregando Spring

---
+++ src/test/java/bo/edu/ucb/ingsof/bot_cine/BotCineApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.ingsof.bot_cine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotCineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
